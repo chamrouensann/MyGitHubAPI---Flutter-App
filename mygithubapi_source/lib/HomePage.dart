d828cedf1f5df041b2d85eebca05e33ae9ef6797
chamrouensann <chamrouensann.geek@gmail.com>
2021-01-01 20:19:53 +0700

    Using github get uers api

--- mygithubapi_source/lib/HomePage.dart
+++ mygithubapi_source/lib/HomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -8,6 +11,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final String url = "https://api.github.com/users";
+  List data;
+  var isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    this.getjsondata();
+  }
+
+  Future<String> getjsondata() async {
+    var response = await http.get(
+      Uri.encodeFull(url),
+    );
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
